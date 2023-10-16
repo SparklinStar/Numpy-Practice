bded3e24f47bb4b2ce5ab13eafbd4673f5b1c39f
ekramzafar <133132379+ekramzafar@users.noreply.github.com>
2023-10-16 22:59:20 +0530

    Update Task1.py

--- Ekram_zafar/Task1.py
+++ Ekram_zafar/Task1.py
@@ -1,1 +1,16 @@
+# Import the numpy library and give it the alias 'np'
+import numpy as np
 
+# 1. Check whether the array is empty
+
+# Create an empty 2x2 array filled with uninitialized integers
+empty_array = np.empty([2, 2], int)
+
+# Print the content of the empty_array
+print(empty_array)
+
+# Print the shape (dimensions) of the empty_array
+print(empty_array.shape)
+
+# Print the total number of elements in the empty_array
+print(empty_array.size)
