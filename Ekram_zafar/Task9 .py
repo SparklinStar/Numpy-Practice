6f0e913c061c4ac01330b5934fa0fa224d5c9a8f
ekramzafar <133132379+ekramzafar@users.noreply.github.com>
2023-10-16 23:44:05 +0530

    Update Task9 .py

--- Ekram_zafar/Task9 .py
+++ Ekram_zafar/Task9 .py
@@ -1,1 +1,8 @@
+#Task: Create array of strings
+import numpy as np
 
+words = ["Apple", "bat", "cat", "dog", "eiffle tower", "friends", "goose", "hands", "ink", "jackle", "kangaroo", "lemon", "man", "nine eleven", "optimus prime", "pinterest", "quarks", "rest", "strings", "time", "ultra", "violet", "world trade center", "xender", "yes", "zibra"] #Random strings according to alphabets
+
+arr = np.random.choice(words, np.random.randint(1, 10))
+
+print(arr)
