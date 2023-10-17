dfdff8b8dc071008e772c5a945cf2f16e51b2cef
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-10-17 22:01:09 +0530

    Create task4.py

---
+++ Piyush/task4.py
@@ -0,0 +1,8 @@
+import numpy as np
+#creating an array with elements 
+array = np.array([[1, 2, 3], [4, 5, 6]])
+#counting the elements of array
+element_count = array.size
+
+print("Number of elements in the array:", element_count)
+#end of task4
