e8b1de1965977f76b356584ce91e901ea50bd210
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-10-19 20:46:14 +0530

    Create task27.py

---
+++ Piyush/task27.py
@@ -0,0 +1,10 @@
+import numpy as np
+a = np.array([[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]])
+flipped_a = np.fliplr(a)
+print("Original array:")
+print(a)
+print("\nHorizontally Flipped array:")
+print(flipped_a)
+#end of 27
