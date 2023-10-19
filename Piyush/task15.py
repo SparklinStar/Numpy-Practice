b2382451d66e5ea4e601ee40f3df3641a85b9af6
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-10-19 19:57:14 +0530

    Create task15.py

---
+++ Piyush/task15.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Create a sample 2D NumPy array
+array = np.array([[1, 2, 3],
+                  [4, 5, 6],
+                  [7, 8, 9]])
+
+# Get the 2nd column (index 1)
+nth_column = array[:, 1]
+
+# Print the result
+print(nth_column)
+#end of task15
