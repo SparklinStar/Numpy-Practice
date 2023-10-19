a56dc7597b20e464f9589add671ee3272087edfa
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-10-19 20:18:29 +0530

    Create task20.py

---
+++ Piyush/task20.py
@@ -0,0 +1,13 @@
+import numpy as np
+a = np.array([[5, 6, 7],
+                [8, 9, 10],
+                [10, 12, 13]])
+
+# Flip the array vertically using numpy.flipud()
+flipped_a = np.flipud(arr)
+
+# Print the original and flipped arrays
+print("Original array:")
+print(a)
+print("\nFlipped array:")
+print(flipped_a)
