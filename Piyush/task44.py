bcc868d3f0e5b97b783086ba4991668540035cc9
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-10-20 22:00:20 +0530

    Create task44.py

---
+++ Piyush/task44.py
@@ -0,0 +1,8 @@
+import numpy as np
+random_array = np.random.randint(low=1, high=100, size=10)
+sorted_array = np.sort(random_array)
+print("Random Integer Array:")
+print(random_array)
+print("Sorted Integer Array:")
+print(sorted_array)
+#end of task44
