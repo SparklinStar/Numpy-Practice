17e4912919124f2c1752dc3c579001425f42c9fd
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-10-20 15:41:43 +0530

    Till 14

---
+++ subinoy-biswas/task4.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+meowarr = np.arange(1,10)
+print(meowarr)
+meowarr1 =np.arange(1.601,5.601)
+print(meowarr1)
---
+++ subinoy-biswas/task9.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+meowarr = ["meow","hi"]
+print(meowarr)
