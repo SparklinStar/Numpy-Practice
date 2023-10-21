e27ebb4ae5f767f7dd189bf570767f15094d3892
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-10-21 22:45:02 +0530

    Create task55.py

---
+++ Piyush/task55.py
@@ -0,0 +1,5 @@
+import numpy as np
+matrix = np.array([[1, 2, 3], [4, 0, 6], [7, 8, 5]])
+min_values_column = np.min(matrix, axis=0)
+print("Minimum values along each column:", min_values_column)
+#end of task55
