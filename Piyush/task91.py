d9e200796a022bde8021fa8f54e8d662fa17fb6c
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-10-26 17:28:35 +0530

    Create task91.py

---
+++ Piyush/task91.py
@@ -0,0 +1,8 @@
+import numpy as np
+original_array = np.array([[1, 2, 3],
+                           [4, 5, 6],
+                           [7, 8, 9]])
+reversed_array = original_array[::-1, ::-1]
+
+print(reversed_array)
+#end of task 91
