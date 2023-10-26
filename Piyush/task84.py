821e7787a0fe04f37193b1c01162520aa3971fff
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-10-26 17:01:18 +0530

    Create task84.py

---
+++ Piyush/task84.py
@@ -0,0 +1,7 @@
+#Numpy array to Pandas Dataframe
+import pandas as pd
+import numpy as np
+numpy_array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+df = pd.DataFrame(numpy_array)
+print(df)
+#end of task84
