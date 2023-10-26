b57c451d51f9848fef1c5a2b4b37f2a9bf6dae62
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-10-26 15:59:39 +0530

    Create task68.py

---
+++ Piyush/task68.py
@@ -0,0 +1,6 @@
+import numpy as np
+arr = np.array([10, 20, 20, 30, 40, 40, 50])
+unique_elements, counts = np.unique(arr, return_counts=True)
+print("Unique Elements:", unique_elements)
+print("Counts:", counts)
+#end of task68
