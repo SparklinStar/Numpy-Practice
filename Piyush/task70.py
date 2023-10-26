6ec213884fbb35fa9486159a52476fd7030689ff
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-10-26 16:03:13 +0530

    Create task70.py

---
+++ Piyush/task70.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# Create two NumPy arrays
+array1 = np.array([10, 20, 3, 40, 50])
+array2 = np.array([3, 4, 50, 60, 70])
+intersection = np.intersect1d(array1, array2)
+print(intersection)
+#end of task70
