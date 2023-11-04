0ed94fbb2ede68f2f639c851a86a61aba52e4c52
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-11-04 17:55:38 +0530

    Create task94.py

---
+++ Piyush/task94.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Create a 2x3 array filled with zeros
+zeros_array = np.zeros((2, 3))
+
+# Create a 3x3 array filled with ones
+ones_array = np.ones((3, 3))
+
+print("Zeros Array:")
+print(zeros_array)
+
+print("Ones Array:")
+print(ones_array)
+#end of task94
