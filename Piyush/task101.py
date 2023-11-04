a7bd9262e0c932b6e564fcd6969571e672fb1028
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-11-04 18:06:22 +0530

    Create task101.py

---
+++ Piyush/task101.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Create two NumPy arrays
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([5, 6, 7, 8])
+
+# Add the two arrays element-wise using vectorization
+result = arr1 + arr2
+
+print("Result of Vectorized Addition:")
+print(result)
+#end of task101
