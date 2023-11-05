a8317f92a6ae0c3e8b7d4dc3e92d39bd4eacd503
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-11-05 17:03:42 +0530

    Create task110.py

---
+++ Piyush/task110.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# Create a sample NumPy array
+arr = np.array([10, 20, 30, 40, 50])
+
+# Define the desired index order
+index_order = np.array([2, 0, 4, 1, 3])
+
+# Rearrange the array based on the specified index order
+rearranged_arr = arr[index_order]
+
+print("Original array:")
+print(arr)
+print("Rearranged array:")
+print(rearranged_arr)
+#end of task110
