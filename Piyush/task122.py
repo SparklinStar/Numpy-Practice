4b682fdee754bda97c0d8d9306d56d17e507ab12
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-11-09 19:09:53 +0530

    Create task122.py

---
+++ Piyush/task122.py
@@ -0,0 +1,12 @@
+#Set Error in Numpy
+import numpy as np
+complex_array = np.array([1 + 2j, 3 - 4j, -2 + 2j])
+
+abs_values = np.abs(complex_array)
+
+print("Complex Array:")
+print(complex_array)
+
+print("Absolute Values (Magnitudes):")
+print(abs_values)
+#end of task122
