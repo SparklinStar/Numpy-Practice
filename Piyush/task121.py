e4b5edfa74494d8441d1e3ac4ba16ffac0617d29
piyush182004 <146834305+piyush182004@users.noreply.github.com>
2023-11-09 19:07:48 +0530

    Create task121.py

---
+++ Piyush/task121.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Create a complex number
+complex_num = 3 + 4j
+
+# Calculate the absolute value (magnitude) of the complex number
+abs_value = np.abs(complex_num)
+
+print("Complex Number:", complex_num)
+print("Absolute Value (Magnitude):", abs_value)
+#end of task121
